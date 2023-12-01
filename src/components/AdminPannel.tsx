5187a6cbd85926e4e76f4dbaeda5d044b735d699
Nicolas Dickreuter <dickreuter@gmail.com>
2023-12-01 08:42:08 +0000

    models

--- src/components/AdminPannel.tsx
+++ src/components/AdminPannel.tsx
@@ -216,8 +216,10 @@ const AdminPannel = () => {
           value={data.selectedModelType}
           onChange={(e) => handleChange("selectedModelType", e.target.value)}
         >
-          <option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k</option>
-          <option value="gpt-4">gpt-4</option>
+          <option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k (£0.02 / request)</option>
+          <option value="gpt-4-1106-preview">gpt-4-1106-preview-128k (£1.00 / request)</option>
+          <option value="gpt-4">gpt-4 8k (£2.00 / request)</option>
+          <option value="gpt-4-32k">gpt-4-32k (£4.00 / request)</option>
         </select>
       </div>
 

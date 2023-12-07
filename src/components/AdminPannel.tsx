e66f24e4d00679fcd2d4fe95b4093ed590e2ac59
Nicolas Dickreuter <dickreuter@gmail.com>
2023-12-07 23:00:23 +0000

    fix model names

--- src/components/AdminPannel.tsx
+++ src/components/AdminPannel.tsx
@@ -218,8 +218,6 @@ const AdminPannel = () => {
         >
           <option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k (£0.02 / request)</option>
           <option value="gpt-4-1106-preview">gpt-4-1106-preview-128k (£0.50 / request)</option>
-          <option value="gpt-4">gpt-4 8k (£1.00 / request)</option>
-          <option value="gpt-4-32k">gpt-4-32k (£2.00 / request)</option>
         </select>
       </div>
 

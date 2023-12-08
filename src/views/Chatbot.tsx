eb3f3d60b2c99640ca76792b53cc867baec71776
Nicolas Dickreuter <dickreuter@gmail.com>
2023-12-08 23:22:14 +0000

    folder uploading

--- src/views/Chatbot.tsx
+++ src/views/Chatbot.tsx
@@ -1,12 +1,19 @@
+import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import { useAuthUser } from "react-auth-kit";
 import { Button, Col, Container, Form, Row, Spinner } from "react-bootstrap";
+import { displayAlert } from "../helper/Alert";
 import { API_URL, HTTP_PREFIX } from "../helper/Constants";
 import withAuth from "../routes/withAuth";
 import "./Chatbot.css";
 
 const Chatbot = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [profileName, setProfileName] = useState("default");
+  const [isUploading, setIsUploading] = useState(false);
+  const [activeDragFolder, setActiveDragFolder] = useState(null);
+
   const [choice, setChoice] = useState("2");
   const [broadness, setBroadness] = useState("1");
   const [dataset, setDataset] = useState("default");
@@ -70,12 +77,51 @@ const Chatbot = () => {
       setChoice("1"); // Set choice to Copilot
 
       // Wait for state update before sending the question
-      await new Promise(resolve => setTimeout(resolve, 0));
+      await new Promise((resolve) => setTimeout(resolve, 0));
       sendQuestion(); // Call sendQuestion function to simulate submit
     }
   };
 
+  const handleDatasetFileDrop = async (collectionName, file) => {
+    // This function will handle the file drop on a specific dataset folder
+    // You can call handleFileSelect here with the dataset name and file
+    handleFileSelect(file, collectionName); // Adjust based on your implementation
+  };
 
+  const handleFileSelect = async (file) => {
+    setSelectedFile(file);
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("profile_name", profileName);
+
+    // Check if profile name is valid
+    if (!/^[a-zA-Z0-9_-]{3,}$/.test(profileName)) {
+      displayAlert(
+        "Profile name should only contain alphanumeric characters and be at least 3 characters long",
+        "warning"
+      );
+      setIsUploading(false);
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `http${HTTP_PREFIX}://${API_URL}/uploadfile/`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${tokenRef.current}`, // Adding the token in the request headers
+          },
+        }
+      );
+      console.log(response.data);
+      displayAlert("Upload successful", "success");
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      displayAlert("Failed to save", "danger");
+    }
+  };
   const sendQuestion = async () => {
     setQuestionAsked(true);
     setResponse("");
@@ -107,17 +153,15 @@ const Chatbot = () => {
 
   const submitFeedback = async () => {
     const formData = new FormData();
-    formData.append('text', `Question: ${inputText} \n Feedback: ${feedback}`);
-    formData.append('profile_name', dataset); // Assuming email is the profile_name
-    formData.append('mode', 'feedback');
-    console.log(formData)
-  
+    formData.append("text", `Question: ${inputText} \n Feedback: ${feedback}`);
+    formData.append("profile_name", dataset); // Assuming email is the profile_name
+    formData.append("mode", "feedback");
+    console.log(formData);
+
     try {
-      await axios.post(
-        `http${HTTP_PREFIX}://${API_URL}/uploadtext`,
-        formData,
-        { headers: { Authorization: `Bearer ${tokenRef.current}` } }
-      );
+      await axios.post(`http${HTTP_PREFIX}://${API_URL}/uploadtext`, formData, {
+        headers: { Authorization: `Bearer ${tokenRef.current}` },
+      });
       // Handle successful submission, e.g., clear feedback or show a message
     } catch (error) {
       console.error("Error sending feedback:", error);
@@ -232,38 +276,71 @@ const Chatbot = () => {
               className="chat-output"
               readOnly
               value={response}
-              onMouseUp={handleTextHighlight} 
+              onMouseUp={handleTextHighlight}
             />
           </Form.Group>
         </Col>
       </Row>
       {/* Feedback Section */}
-{/* Feedback Section */}
-<Row className="justify-content-md-center">
-  <Col md={12}>
-    <Form.Group className="mb-3">
-      <Form.Label>Feedback: (describe how the question can be answered better in the future) </Form.Label>
-      <Form.Control
-        as="textarea"
-        className="feedback-textarea"
-        value={feedback}
-        onChange={(e) => setFeedback(e.target.value)}
-        disabled={!questionAsked} // Disabled until a question is asked
-      />
-    </Form.Group>
-    <div className="d-flex justify-content-center mb-3">
-      <Button
-        variant="primary"
-        onClick={submitFeedback}
-        className="chat-button"
-        disabled={!questionAsked} // Disabled until a question is asked
-      >
-        Submit Feedback
-      </Button>
-    </div>
-  </Col>
-</Row>
-
+      {/* Feedback Section */}
+      <Row className="justify-content-md-center">
+        <Col md={12}>
+          <Form.Group className="mb-3">
+            <Form.Label>
+              Feedback: (describe how the question can be answered better in the
+              future){" "}
+            </Form.Label>
+            <Form.Control
+              as="textarea"
+              className="feedback-textarea"
+              value={feedback}
+              onChange={(e) => setFeedback(e.target.value)}
+              disabled={!questionAsked} // Disabled until a question is asked
+            />
+          </Form.Group>
+          <div className="d-flex justify-content-center mb-3">
+            <Button
+              variant="primary"
+              onClick={submitFeedback}
+              className="chat-button"
+              disabled={!questionAsked} // Disabled until a question is asked
+            >
+              Submit Feedback
+            </Button>
+          </div>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center mt-4">
+    <Col md={12}>
+      <div className="dataset-folders">
+        {availableCollections.map((collection, index) => (
+          <div 
+            key={index} 
+            className={`dataset-folder ${activeDragFolder === collection ? 'drag-over' : ''}`}
+            onDragEnter={e => {
+              e.preventDefault();
+              setActiveDragFolder(collection);
+            }}
+            onDragOver={e => {
+              e.preventDefault();
+            }}
+            onDragLeave={e => {
+              e.preventDefault();
+              setActiveDragFolder(null);
+            }}
+            onDrop={e => {
+              e.preventDefault();
+              setActiveDragFolder(null);
+              const file = e.dataTransfer.files[0];
+              handleDatasetFileDrop(collection, file);
+            }}
+          >
+            <CreateNewFolderIcon /> <span>{collection}</span>
+          </div>
+        ))}
+      </div>
+    </Col>
+  </Row>
     </Container>
   );
 };

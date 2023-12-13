216dc654318d6f7900b221172ea033fceccdf359
Nicolas Dickreuter <dickreuter@gmail.com>
2023-12-13 10:40:27 +0000

    delete feedback

--- src/components/FLog.tsx
+++ src/components/FLog.tsx
@@ -34,6 +34,29 @@ const FLog = () => {
     fetchLogs();
   }, []);
 
+  const handleDelete = async (entryId) => {
+    // Confirmation pop-up
+    if (window.confirm("Are you sure you want to delete this entry?")) {
+      try {
+        // Using FormData to send data
+        const formData = new FormData();
+        formData.append('entry_id', entryId);
+  
+        await axios.post(`http${HTTP_PREFIX}://${API_URL}/delete_feedback`, formData, {
+          headers: {
+            Authorization: `Bearer ${tokenRef.current}`,
+            'Content-Type': 'multipart/form-data', // This header is important for FormData
+          },
+        });
+  
+        // Remove the deleted log from the state
+        setLogs(logs.filter(log => log._id !== entryId));
+      } catch (error) {
+        console.error('Error deleting log:', error);
+      }
+    }
+  };
+  
   return (
     <div className="log-table">
       <table>
@@ -42,6 +65,7 @@ const FLog = () => {
             {columns.map((column, index) => (
               <th key={index}>{column}</th>
             ))}
+            <th>Delete</th> {/* Header for delete actions */}
           </tr>
         </thead>
         <tbody>
@@ -50,6 +74,9 @@ const FLog = () => {
               {columns.map((column, cellIndex) => (
                 <td key={cellIndex}>{log[column]}</td>
               ))}
+              <td>
+                <button onClick={() => handleDelete(log._id)}>X</button> {/* Delete button */}
+              </td>
             </tr>
           ))}
         </tbody>

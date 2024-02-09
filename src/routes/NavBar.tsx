d61f798761657716b4db0b937f5ce0c5cb972be9
Alex Hoyle <alexanderhoyle123@gmail.com>
2024-02-09 15:45:21 +0000

    login page done

--- src/routes/NavBar.tsx
+++ src/routes/NavBar.tsx
@@ -158,6 +158,15 @@ const NavBar = () => {
           ) : (
             <></>
           )}
+           {auth ? (
+            <li className="nav-item">
+              <Link className="nav-link" to="/bids">
+                Bids
+              </Link>
+            </li>
+          ) : (
+            <></>
+          )}
           {auth ? (
             email === "adminuser" && (
               <>

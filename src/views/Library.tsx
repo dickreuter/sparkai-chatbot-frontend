07e81dac4b16fa793b3692a81642f2ec98a6b774
Alex Hoyle <alexanderhoyle123@gmail.com>
2024-02-09 17:06:22 +0000

    sidebar

--- src/views/Library.tsx
+++ src/views/Library.tsx
@@ -3,14 +3,19 @@ import { API_URL, HTTP_PREFIX } from '../helper/Constants';
 import axios from 'axios';
 import withAuth from '../routes/withAuth';
 import { useAuthUser } from 'react-auth-kit';
+<<<<<<< HEAD
 import {Button, Col, Container, Form, Row, Spinner, Card} from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import UploadPDF from './UploadPDF';
 import "./Library.css";
+=======
+
+>>>>>>> 20f51b1 (sidebar)
 
 const Library = () => {
     return (
         <div className="App">
+<<<<<<< HEAD
 
           <h1>Library</h1>
           <Link to="/">
@@ -82,6 +87,53 @@ const Library = () => {
 
           </div>
           
+=======
+          <h1>Library</h1>
+          <table className="bids-table">
+            <thead>
+              <tr>
+                <th>Company Library</th>
+                <th>History</th>
+                <th>Upload Text</th>
+                <th>Upload PDF</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Staff Structure</td>
+                <td>Last opened: 07/02/2024</td>
+                <td>View</td>
+                <td>Delete</td>
+              </tr>
+              {/* Additional dummy bids */}
+              <tr>
+                <td>Bid Structure</td>
+                <td>Last opened: 06/02/2024</td>
+                <td>View</td>
+                <td>Delete</td>
+              </tr>
+              <tr>
+                <td> Company Culture</td>
+                <td>Last opened: 05/02/2024</td>
+                <td>View</td>
+                <td>Delete</td>
+              </tr>
+              <tr>
+                <td>Sales Material</td>
+                <td>Last opened: 04/02/2024</td>
+                <td>View</td>
+                <td>Delete</td>
+              </tr>
+              <tr>
+                <td>GANT Chart</td>
+                <td>Last opened: 03/02/2024</td>
+                <td>View</td>
+                <td>Delete</td>
+              </tr>
+              {/* Continue adding more dummy bids as needed */}
+            </tbody>
+          </table>
+>>>>>>> 20f51b1 (sidebar)
         </div>
       );
 }

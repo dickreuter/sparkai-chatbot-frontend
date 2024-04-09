79f4414cfc840c72dcaffa93e181d1e1d2df901b
Nicolas Dickreuter <dickreuter@gmail.com>
2024-04-09 23:25:58 +0700

    remove copilot items

--- src/routes/Sidebar.tsx
+++ src/routes/Sidebar.tsx
@@ -158,22 +158,18 @@ const SideBar = ({ isCopilotVisible, setIsCopilotVisible, selectedText, askCopil
                 <div className="sidebar-links smaller-links">
                 <a href="#" onClick={handleLinkClick('Expand')} className='sidebar-link'>Expand</a>
                 <a href="#" onClick={handleLinkClick('Summarise')} className='sidebar-link'>Summarise</a>
-                <a href="#" onClick={handleLinkClick('Improve Grammar')} className='sidebar-link'>Improve Grammar</a>
                 <a href="#" onClick={handleLinkClick('Translate to English')} className='sidebar-link'>Translate to English</a>
-                <a href="#" onClick={handleLinkClick('Change Tense')} className='sidebar-link'>Change Tense</a>
                 <a href="#" onClick={handleLinkClick('Rephrase')} className='sidebar-link'>Rephrase</a>
                 <a href="#" onClick={handleLinkClick('Incorporate')} className='sidebar-link'>Incorporate</a>
                 <a href="#" onClick={handleLinkClick('We Will Active Voice')} className='sidebar-link'>We Will Active Voice</a>
                 <a href="#" onClick={handleLinkClick('List Creator')} className='sidebar-link'>List Creator</a>
                 <a href="#" onClick={handleLinkClick('Reduce Word Character Count')} className='sidebar-link'>Reduce Word Character Count</a>
-                <a href="#" onClick={handleLinkClick('Word Cutting Adverbs')} className='sidebar-link'>Word Cutting Adverbs</a>
                 <a href="#" onClick={handleLinkClick('Word Cutting Adjectives')} className='sidebar-link'>Word Cutting Adjectives</a>
                 <a href="#" onClick={handleLinkClick('Word Cutting Commas with Dashes')} className='sidebar-link'>Word Cutting Commas with Dashes</a>
                 <a href="#" onClick={handleLinkClick('Explain How')} className='sidebar-link'>Explain How</a>
                 <a href="#" onClick={handleLinkClick('Add Statistics')} className='sidebar-link'>Add Statistics</a>
                 <a href="#" onClick={handleLinkClick('For Example')} className='sidebar-link'>For Example</a>
                 <a href="#" onClick={handleLinkClick('Adding Case Study')} className='sidebar-link'>Adding Case Study</a>
-                <a href="#" onClick={handleLinkClick('Search Company Library')} className='sidebar-link'>Search Company Library</a>
                 </div>
                 
             )}

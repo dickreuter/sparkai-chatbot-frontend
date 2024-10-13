be1e57c7c65b400b3789e184e4537cb6e2d18202
Alex <alexanderhoyle123@gmail.com>
2024-10-13 16:25:53 +0100

    made a primitive wordpannel with authentication

---
+++ wordpane/src/taskpane/components/auth/UseAuthsignIn.tsx
@@ -0,0 +1,53 @@
+// useAuthSignIn.js
+import { useState } from 'react';
+import axios from 'axios';
+import { useSignIn } from 'react-auth-kit';
+import { useNavigate } from 'react-router-dom';
+import { API_URL, HTTP_PREFIX } from '../../helper/Constants';
+
+const useAuthSignIn = () => {
+  const signIn = useSignIn();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<any | null>(null);
+
+  const submitSignIn = async (formData) => {
+    setIsLoading(true);
+    console.log("auth submit signin");
+    try {
+      const res = await axios.post(`http${HTTP_PREFIX}://${API_URL}/login`, formData);
+      setIsLoading(false);
+      console.log(res.status);
+      console.log("posted signin");
+      if (res.status === 200 && signIn({
+        token: res.data.token,
+        expiresIn: 3600 * 48,
+        tokenType: "Bearer",
+        authState: {
+          email: res.data.email,
+          token: res.data.access_token,
+        }
+      })) {
+        navigate('/');
+        return { success: true, message: 'Log in successful!' };
+      } else {
+        setError('Log in unsuccessful');
+        console.log("log in unsuccessful"); 
+        return { success: false, message: 'Log in unsuccessful' };
+      }
+    } catch (err) {
+      setIsLoading(false);
+      setError(err.message || 'An error occurred. Please try again.');
+      console.log(err.message);
+      return { success: false, message: err.message || 'An error occurred. Please try again.' };
+    }
+  };
+
+  return {
+    submitSignIn,
+    isLoading,
+    error,
+  };
+};
+
+export default useAuthSignIn;
--- wordpane/webpack.config.js
+++ wordpane/webpack.config.js
@@ -4,108 +4,125 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
-
+const dotenv = require('dotenv');
 const urlDev = "https://localhost:3000/";
 const urlProd = "https://app.mytender.io:3000"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {});
+
 
 async function getHttpsOptions() {
-  const httpsOptions = await devCerts.getHttpsServerOptions();
-  return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
+    const httpsOptions = await devCerts.getHttpsServerOptions();
+    return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
 }
 
-module.exports = async (env, options) => {
-  const dev = options.mode === "development";
-  const config = {
-    devtool: "source-map",
-    entry: {
-      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      vendor: ["react", "react-dom", "core-js", "@fluentui/react-components", "@fluentui/react-icons"],
-      taskpane: ["./src/taskpane/index.tsx", "./src/taskpane/taskpane.html"],
-      commands: "./src/commands/commands.ts",
-    },
-    output: {
-      clean: true,
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".html", ".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-typescript"],
-            },
-          },
+module.exports = async(env, options) => {
+    const dev = options.mode === "development";
+    const config = {
+        devtool: "source-map",
+        entry: {
+            polyfill: ["es6-promise/auto", "core-js/stable", "regenerator-runtime/runtime"],
+            vendor: ["react", "react-dom", "core-js", "@fluentui/react-components", "@fluentui/react-icons"],
+            taskpane: ["./src/taskpane/index.tsx", "./src/taskpane/taskpane.html"],
+            commands: "./src/commands/commands.ts",
         },
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          use: ["ts-loader"],
+        output: {
+            clean: true,
         },
-        {
-          test: /\.html$/,
-          exclude: /node_modules/,
-          use: "html-loader",
+        resolve: {
+            extensions: [".ts", ".tsx", ".html", ".js"],
+            fallback: {
+                "promise": require.resolve("es6-promise"),
+                crypto: false
+            }
         },
-        {
-          test: /\.(png|jpg|jpeg|ttf|woff|woff2|gif|ico)$/,
-          type: "asset/resource",
-          generator: {
-            filename: "assets/[name][ext][query]",
-          },
+        module: {
+            rules: [{
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-typescript"],
+                        },
+                    },
+                },
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: ["ts-loader"],
+                },
+                {
+                    test: /\.html$/,
+                    exclude: /node_modules/,
+                    use: "html-loader",
+                },
+                {
+                    test: /\.(png|jpg|jpeg|ttf|woff|woff2|gif|ico)$/,
+                    type: "asset/resource",
+                    generator: {
+                        filename: "assets/[name][ext][query]",
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+            ],
         },
-      ],
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: "assets/*",
-            to: "assets/[name][ext][query]",
-          },
-          {
-            from: "manifest*.xml",
-            to: "[name]" + "[ext]",
-            transform(content) {
-              if (dev) {
-                return content;
-              } else {
-                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
-              }
-            },
-          },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [{
+                        from: "assets/*",
+                        to: "assets/[name][ext][query]",
+                    },
+                    {
+                        from: "manifest*.xml",
+                        to: "[name]" + "[ext]",
+                        transform(content) {
+                            if (dev) {
+                                return content;
+                            } else {
+                                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
+                            }
+                        },
+                    },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                filename: "taskpane.html",
+                template: "./src/taskpane/taskpane.html",
+                chunks: ["polyfill", "vendor", "taskpane"],
+            }),
+            new HtmlWebpackPlugin({
+                filename: "commands.html",
+                template: "./src/commands/commands.html",
+                chunks: ["commands"],
+            }),
+            new webpack.ProvidePlugin({
+                Promise: ["es6-promise", "Promise"],
+            }),
+            new webpack.DefinePlugin(envKeys)
         ],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "taskpane.html",
-        template: "./src/taskpane/taskpane.html",
-        chunks: ["polyfill", "vendor", "taskpane"],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["commands"],
-      }),
-      new webpack.ProvidePlugin({
-        Promise: ["es6-promise", "Promise"],
-      }),
-    ],
-    devServer: {
-      hot: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      server: {
-        type: "https",
-        options: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
-      },
-      port: process.env.npm_package_config_dev_server_port || 3000,
-    },
-  };
+        devServer: {
+            hot: true,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            server: {
+                type: "https",
+                options: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
+            },
+            port: process.env.npm_package_config_dev_server_port || 3000,
+        },
+    };
 
-  return config;
+    return config;
 };

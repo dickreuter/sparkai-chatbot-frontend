3fe40847600105ed76592a8e418c7657ae88e06b
Amos Garcia <amosraga21@gmail.com>
2024-10-31 20:48:32 -0500

    feat: update add-in branding and functionality

--- wordpane/src/taskpane/components/auth/SignIn.tsx
+++ wordpane/src/taskpane/components/auth/SignIn.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Signin.css";
-import { Alert, Button, Snackbar, TextField, Modal } from "@mui/material";
+import { Alert, Button, Snackbar, TextField, Modal, Box, Link, Typography } from "@mui/material";
 import useAuthSignIn from "./UseAuthsignIn";
 import AuthState from "./AuthState";
 import axios from "axios";
@@ -71,88 +71,102 @@ const FullScreenTwoCards = () => {
   };
 
   return (
-    <div className="cards-container">
-      <div className="cardmini">
-        <div className="cardmini-text">
-          <h2>Login</h2>
+    <div className="page-signin">
+      <div className="page-header">
+        <Typography component={"span"}>This add-in will connect to </Typography>
+        <Link href="https://www.mytender.io" target="_blank">
+          mytender.io
+        </Link>
+        <Typography component={"span"}> and help you win tenders by using artificial intelligence.</Typography>
+      </div>
+      <div className="card-container">
+        <div className="cardmini">
+          <div className="cardmini-text">
+            <h2>Login</h2>
 
-          <div className="input-field">
-            <TextField
-              id="email-input"
-              fullWidth
-              label="Enter your username"
-              variant="outlined"
-              value={formData.email}
-              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-              onKeyPress={handleKeyPress}
-              InputProps={inputProps}
-              InputLabelProps={labelProps}
-              autoComplete="username"
-            />
-          </div>
-          <div className="input-field">
-            <TextField
-              id="password-input"
-              fullWidth
-              label="Password"
-              variant="outlined"
-              type="password"
-              value={formData.password}
-              onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-              onKeyPress={handleKeyPress}
-              InputProps={inputProps}
-              InputLabelProps={labelProps}
-              autoComplete="current-password"
-            />
-          </div>
-          <Button className="login-button" variant="contained" onClick={onSubmit} disabled={isLoading}>
-            {isLoading ? "Loading..." : "Login"}
-          </Button>
-          <p style={{ cursor: "pointer" }} onClick={() => setForgotPasswordOpen(true)}>
-            Forgot your password?
-          </p>
-        </div>
-
-        <Snackbar
-          open={snackbarOpen}
-          autoHideDuration={3000}
-          onClose={() => setSnackbarOpen(false)}
-          style={{
-            position: "fixed",
-            bottom: "-25%",
-            marginBottom: "15px",
-          }}
-        >
-          <Alert onClose={() => setSnackbarOpen(false)} severity={snackbarSeverity}>
-            {snackbarMessage}
-          </Alert>
-        </Snackbar>
-
-        <Modal open={forgotPasswordOpen} onClose={() => setForgotPasswordOpen(false)}>
-          <div className="modal-container">
-            <h2>Forgot Password</h2>
-            <p>Enter your email address and we'll send you a link to reset your password.</p>
-            <TextField
-              autoFocus
-              margin="dense"
-              label="Email Address"
-              type="email"
-              fullWidth
-              value={forgotPasswordEmail}
-              onChange={(e) => setForgotPasswordEmail(e.target.value)}
-            />
-            <div className="modal-actions">
-              <Button onClick={() => setForgotPasswordOpen(false)} color="primary">
-                Cancel
-              </Button>
-              <Button onClick={handleForgotPassword} color="primary">
-                Send Email
-              </Button>
+            <div className="input-field">
+              <TextField
+                id="email-input"
+                fullWidth
+                label="Enter your username"
+                variant="outlined"
+                value={formData.email}
+                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                onKeyPress={handleKeyPress}
+                InputProps={inputProps}
+                InputLabelProps={labelProps}
+                autoComplete="username"
+              />
             </div>
+            <div className="input-field">
+              <TextField
+                id="password-input"
+                fullWidth
+                label="Password"
+                variant="outlined"
+                type="password"
+                value={formData.password}
+                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                onKeyPress={handleKeyPress}
+                InputProps={inputProps}
+                InputLabelProps={labelProps}
+                autoComplete="current-password"
+              />
+            </div>
+            <Button className="login-button" variant="contained" onClick={onSubmit} disabled={isLoading}>
+              {isLoading ? "Loading..." : "Login"}
+            </Button>
+            <Box sx={{ display: "flex" }} justifyContent={"space-between"}>
+              <p style={{ cursor: "pointer" }} onClick={() => setForgotPasswordOpen(true)}>
+                Forgot your password?
+              </p>
+              <Link href="https://www.mytender.io" target="_blank" style={{ marginTop: "8px" }}>
+                Sign up
+              </Link>
+            </Box>
           </div>
-        </Modal>
 
-        <AuthState />
+          <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={3000}
+            onClose={() => setSnackbarOpen(false)}
+            style={{
+              position: "fixed",
+              bottom: "-25%",
+              marginBottom: "15px",
+            }}
+          >
+            <Alert onClose={() => setSnackbarOpen(false)} severity={snackbarSeverity}>
+              {snackbarMessage}
+            </Alert>
+          </Snackbar>
+
+          <Modal open={forgotPasswordOpen} onClose={() => setForgotPasswordOpen(false)}>
+            <div className="modal-container">
+              <h2>Forgot Password</h2>
+              <p>Enter your email address and we'll send you a link to reset your password.</p>
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Email Address"
+                type="email"
+                fullWidth
+                value={forgotPasswordEmail}
+                onChange={(e) => setForgotPasswordEmail(e.target.value)}
+              />
+              <div className="modal-actions">
+                <Button onClick={() => setForgotPasswordOpen(false)} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={handleForgotPassword} color="primary">
+                  Send Email
+                </Button>
+              </div>
+            </div>
+          </Modal>
+
+          <AuthState />
+        </div>
       </div>
     </div>
   );

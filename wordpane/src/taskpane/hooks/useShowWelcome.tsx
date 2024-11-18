a49745f839d34c012d5ef1366160f2153d4faa9d
Amos Garcia <amosraga@outlook.com>
2024-11-18 18:42:34 +0200

    Fix: welcome screen appearing issue (#115)
    fix: welcome screen appearing issue

--- wordpane/src/taskpane/hooks/useShowWelcome.tsx
+++ wordpane/src/taskpane/hooks/useShowWelcome.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 
 export default function useShowWelcome() {
-  const [showWelcome, setShowWelcome] = useState<boolean>(localStorage.getItem("showWelcome") === "true");
+  const [showWelcome, _setShowWelcome] = useState<boolean>(
+    localStorage.getItem("showWelcome") === "false" ? false : true
+  );
 
   useEffect(() => {
     localStorage.setItem("showWelcome", showWelcome.toString());
   }, [showWelcome]);
+
+  const setShowWelcome = (show: boolean) => {
+    _setShowWelcome(show);
+    localStorage.setItem("showWelcome", show.toString());
+  };
 
   return { showWelcome, setShowWelcome };
 }

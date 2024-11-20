2ac6d47164a285984c242a63c95b1eb728861d0f
Amos Garcia <amosraga21@gmail.com>
2024-11-19 18:25:19 -0600

    feat: ui clean ups

--- wordpane/src/taskpane/components/MessageBox/Creator.tsx
+++ wordpane/src/taskpane/components/MessageBox/Creator.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { IMessage } from "../../../types";
-import { PermIdentityRounded as PermIdentityRoundedIcon } from "@mui/icons-material";
 import { Typography } from "@mui/material";
 
 interface CreatorProps {
@@ -8,11 +7,12 @@ interface CreatorProps {
 }
 
 const Creator = ({ message }: CreatorProps) => {
+  const isUser = useMemo(() => message.createdBy === "user", [message.createdBy]);
   return (
     <div className="creator">
-      {message.createdBy === "user" ? <PermIdentityRoundedIcon /> : <img src="/assets/logo.png" />}
-      <Typography variant="caption" display="block" fontWeight="bold">
-        {message.createdBy === "user" ? "You" : "mytender.io"}
+      {message.createdBy === "user" ? <img src="/assets/user.svg" /> : <img src="/assets/logo.png" />}
+      <Typography variant="caption" display="block" fontWeight={isUser ? "normal" : "bold"}>
+        {isUser ? "You" : "mytender.io"}
       </Typography>
     </div>
   );
